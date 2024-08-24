3fac2cbcd891ddd009a582f2aed23f28267e7c38
Miguel <ml5598016@gmail.com>
2024-08-23 23:55:24 -0600

    Commit desde terminal

---
+++ app.js
@@ -0,0 +1,5 @@
+let  nombre = prompt("Ingresa tu nombre");
+let edad = parseInt(prompt("Ingresa tu edad: "));
+console.log("Tu nombre es: "+ nombre);
+console.log("Tu edad es: "+ edad);
+console.log(typeof edad);
