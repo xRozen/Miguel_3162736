95d189ec12ee5a25ab80d17b4ff96d8786798720
Miguel <ml5598016@gmail.com>
2024-09-02 23:09:52 -0600

    Funcion suma

---
+++ Funcion_suma/app4.js
@@ -0,0 +1,9 @@
+let num1 = parseInt(prompt("Ingresa el primer numero"));
+let num2 = parseInt(prompt("Ingresa el segundo numero"));
+
+function suma(a,b){
+    return (a + b);
+}
+
+resultado = suma(num1,num2);
+console.log(resultado);
