0a19395150454c00bc5ec39aef59148a6baaa32b
Miguel <ml5598016@gmail.com>
2024-10-18 23:41:19 -0600

    Realizacion de una calculadora

---
+++ Calculadora/calculadora.js
@@ -0,0 +1,17 @@
+
+function agregarPantalla(value) {
+    document.getElementById("pantalla").value += value;
+}
+
+function limpiarPantalla() {
+    document.getElementById("pantalla").value="";
+}
+
+function calcular(){
+    try{
+        let result = eval(document.getElementById ("pantalla").value);
+        document.getElementById("pantalla").value = result;
+    }catch(error) {
+        document .getElementById("pantalla").value = "Error";
+    }
+}
